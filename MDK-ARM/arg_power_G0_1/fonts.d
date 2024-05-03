88ad56ed36a0e1cf0f68df355b57500990a1e741
adcNoise <49891693+adcNoise@users.noreply.github.com>
2024-05-03 13:21:26 +0800

    Удалены *.O и *.D

--- MDK-ARM/arg_power_G0_1/fonts.d
+++
@@ -1,3 +0,0 @@
-arg_power_g0_1/fonts.o: ..\Core\Src\fonts.c ..\Core\Inc\fonts.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\inttypes.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h
--- MDK-ARM/arg_power_G0_1/stm32g0xx_ll_rcc.d
+++
@@ -1,2 +0,0 @@
-arg_power_g0_1/stm32g0xx_ll_rcc.o: \
-  ..\Drivers\STM32G0xx_HAL_Driver\Src\stm32g0xx_ll_rcc.c
